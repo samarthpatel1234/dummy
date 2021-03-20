44b0076c79d470cec8a13ce3e35dd7ab67087166
samarth <samarth@digiqt.com>
2021-03-20 11:35:06 +0530

    second Commit

--- dummayapp/views.py
+++ dummayapp/views.py
@@ -4,7 +4,7 @@ from dummayapp import serializer
 from django.http import HttpResponse
 
 class DummyViewSet(APIView):
-    def post(self, request):
+    def get(self, request):
         serializer_instance = serializer.serializer(data= request.data) 
         if serializer_instance.is_valid():
             return HttpResponse(serializer_instance.validated_data.get("data"))
